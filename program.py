212cdafd04fd5aa95f126ee1b356b2b93a2049b7
Kynan Stevenson <kynants@gmail.com>
2020-10-14 07:28:07 -0400

    Initial commit

---
+++ program.py
@@ -0,0 +1,13 @@
+# MY ATTEMPT
+# print("a string that you \"don't\" have to escape")
+# print("This \n"
+#       "is a ....... multi-line")
+# print("heredoc string --------> example")
+
+# THE SOLUTION AFTER I LEARNED ABOUT HERE-DOCUMENTS
+print("""
+a string that you "don't" have to escape
+This
+is a  ....... multi-line
+heredoc string --------> example
+""")
